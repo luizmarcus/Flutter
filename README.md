da1577dad05d114f1e4350f022020341fa91fe5d
luizmarcus <luizm1000@hotmail.com>
2019-05-01 20:03:50 -0300

    Exemplo "Olá mundo" em Flutter

---
+++ README.md
@@ -0,0 +1,5 @@
+Exemplos de Projetos em Flutter
+==================
+
+
+Este repositório contém todos os projetos em Flutter que serviram como exemplo para as postagens no blog [luizmarcus.com](http://luizmarcus.com)
---
+++ ola_mundo/lib/main.dart
@@ -0,0 +1,21 @@
+import 'package:flutter/material.dart';
+
+void main() => runApp(MyApp());
+
+class MyApp extends StatelessWidget {
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: "Olá Mundo",
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text('Primeiro Projeto'),
+        ),
+        body: Center(
+          child: Text('Olá Mundo!'),
+        ),
+      ),
+    );
+  }
+}
